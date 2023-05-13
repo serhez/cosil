2822ed9251ad30ea911da8ab4f7e4bfbd2558b92
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-05-13 15:06:09 +0300

    feat: adapt experiments bash script to triton script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,3 +1,12 @@
+#!/bin/bash -l
+#SBATCH --time=06:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+module restore cosil
+source activate cosil
+
 for experiment in range-mean-0 range-mean-100 range-min-0 range-min-100 zscore-mean-0 zscore-mean-100 zscore-min-0 zscore-min-100
 do
     echo "Running experiment: $experiment"
