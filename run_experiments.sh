ebefbeaa2a8ace4fb5c657ee26b24c823b3a0231
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-05-15 15:39:14 +0300

    chore: improve environment setup

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
-#SBATCH --time=06:00:00
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=32G
+#SBATCH --time=55:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 
 module restore cosil
 source activate cosil
 
-for experiment in range-mean-0 range-mean-100 range-min-0 range-min-100 zscore-mean-0 zscore-mean-100 zscore-min-0 zscore-min-100
+for experiment in range-mean-1 range-mean-100 range-min-1 range-min-100 zscore-mean-1 zscore-mean-100 zscore-min-1 zscore-min-100
 do
     echo "Running experiment: $experiment"
-    python train.py +experiment=11-05-2023/normalization/"$experiment"
+    srun python train.py +experiment=11-05-2023/normalization/"$experiment"
 done
 
 echo "Done!"
