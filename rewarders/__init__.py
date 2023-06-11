3e47f7ec5e5687191e815dd053be909ef7a1ab08
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-06-11 18:36:44 +0300

    fix: importing removed rewarder PWIL

--- rewarders/__init__.py
+++ rewarders/__init__.py
@@ -2,6 +2,5 @@ from .dual_rewarder import DualRewarder
 from .env_reward import EnvReward
 from .gail import GAIL
 from .mbc import MBC
-from .pwil import PWIL
 from .rewarder import Rewarder
 from .sail import SAIL
