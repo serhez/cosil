8291afb41d6c863154dcb3c0ada5403dfb52d5f1
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-07-09 13:27:30 +0300

    chore: removed large files

---
+++ scripts/run_79431.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --time=50:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=8G
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=sergio.hernandezgutierrez@aalto.fi
+#SBATCH --mail-type=ALL
+
+module restore cosil
+source activate cosil
+
+srun python train.py +experiment=pt-om00/baseline ++seed=79431
