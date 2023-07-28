e8817b15a384bf0e083b1a1182773709d79d8257
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-07-28 14:44:04 +0300

    chore: prepare experiment scripts

--- scripts/run_214.sh
+++ scripts/run_214.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --time=70:00:00
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=sergio.hernandezgutierrez@aalto.fi
 #SBATCH --mail-type=ALL
@@ -9,4 +9,4 @@
 module restore cosil
 source activate cosil
 
-srun python train.py +experiment=13-07-2023/gail/om04-ss-repdemos-skvel ++seed=214
+srun python train.py +experiment=25-07-2023/experiment/baseline/baseline ++seed=214
