5c0ab97b9d891c76250a47ef862a8b329dd75fb5
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-07-29 23:54:30 +0300

    chore: experiments

--- scripts/run_111.sh
+++ scripts/run_111.sh
@@ -9,4 +9,4 @@
 module restore cosil
 source activate cosil
 
-srun python train.py +experiment=25-07-2023/experiment/om001-zsrew-disctrans ++seed=111
+srun python train.py +experiment=25-07-2023/experiment/airl/om001-zsrew-disctrans ++seed=111
