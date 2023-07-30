6dea13f4646377859d5c8deb61aeca92499f9476
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-07-30 12:37:50 +0300

    chore: adjust RAM usage

--- scripts/run_372.sh
+++ scripts/run_372.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --time=50:00:00
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=24G
+#SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=sergio.hernandezgutierrez@aalto.fi
 #SBATCH --mail-type=ALL
