f551a9b736819d4564559873a91f04a2398cb224
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-08-10 12:21:06 +0100

    chore: experiments

--- scripts/run_123456.sh
+++ scripts/run_123456.sh
@@ -9,4 +9,4 @@
 module restore cosil
 source activate cosil
 
-srun python train.py +experiment=final/pretrain/sail/om02-zsrew-disctrans-pos-pso ++seed=123456
+srun python train.py +experiment=final/pretrain/sail/om02-zsrew-disctrans-vel-pso ++seed=123456
