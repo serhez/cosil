99a859ceabcc21823827a378cf67abb998edfcdc
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-12-26 12:36:41 +0000

    chore: experiments

--- scripts/run_123456.sh
+++ scripts/run_123456.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --time=01:30:00
+#SBATCH --time=80:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
@@ -11,4 +11,4 @@ source activate cosil
 
 export MUJOCO_GL="egl"
 
-xvfb-run python train.py +experiment=scaledhumanoid/pretrain/baseline/baseline-pso-zs ++seed=123456
+xvfb-run python train.py +experiment=scaledhumanoid/experiment/baseline/baseline-pso-zs ++seed=123456
