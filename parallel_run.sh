b4076ba60d0fe6d4759d0196207b3311255ac21b
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-01-03 14:48:47 +0000

    chore: experiments

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -l
-#SBATCH --time=80:00:00
+#SBATCH --time=60:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH --mail-user=sergio.hernandezgutierrez@aalto.fi
 #SBATCH --mail-type=ALL
-#SBATCH --array=0-4
+#SBATCH --array=0-1
 
 case $SLURM_ARRAY_TASK_ID in
-   0)  SEED=111 ;;
-   1)  SEED=123456  ;;
-   2)  SEED=12417  ;;
-   3)  SEED=13  ;;
-   4)  SEED=214 ;;
+   0)  SEED=289 ;;
+   1)  SEED=3 ;;
 esac
 
 module restore cosil
@@ -20,4 +17,4 @@ source activate cosil
 
 export MUJOCO_GL="egl"
 
-xvfb-run python train.py +experiment=humanoid/experiment/sail/om02-zsrew-disctrans-pso ++seed=$SEED
+xvfb-run python train.py +experiment=humanoid/experiment/baseline/baseline-pso-zs ++seed=$SEED
